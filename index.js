2abe1c1732054b273f840389351015e0dfe6929c
ashlee.larrick <ashleemareelarrick323@gmail.com>
2023-01-25 11:11:59 -0700

    Passed

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// code your solution here
+function superbowlWin(wins) {
+    let result = wins.find(element => element['result'] == "W")
+    if(result === undefined) {
+        return result
+    }
+    else {
+        return result.year
+    }
+}
+
+let wins = [
+    {"year" : 2015, "result" : "W"},
+    {'year' : 2010, "result" : "L"}
+]
+superbowlWin(wins)
